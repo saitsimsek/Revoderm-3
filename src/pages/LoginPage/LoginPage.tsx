a87d77d23ac7d9afee6405f97dee92c82499660f
sait.simsek <sait.simsek@saglik.gov.tr>
2026-03-01 17:12:49 +0300

    asdasdas

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -18,6 +18,11 @@ export const LoginPage = () => {
     setError(null)
     setSubmitting(true)
     console.log("abc")
+    console.log("abc")
+    console.log("abc")
+    console.log("abc")
+    console.log("abc")
+    console.log("abc")
     try {
       await login(email, password)
       navigate(ROUTES.DASHBOARD)
