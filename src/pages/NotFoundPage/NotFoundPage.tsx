ca565dd7f6fe14b57736f97801529154b13e4db3
saitsimsek <saitsimsek93@gmail.com>
2026-03-02 16:04:18 +0300

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom'
 import { ROUTES } from '../../constants/routes'
 
 export const NotFoundPage = () => (
+
   <main className="page">
     <h1>404</h1>
     <p>Page not found.</p>
     <Link className="button" to={ROUTES.HOME}>
       Back to Home
+       Back to Home
     </Link>
   </main>
 )
