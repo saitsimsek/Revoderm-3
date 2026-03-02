e151af91a27c5c68e1e59e918f7961a62ac7066f
saitsimsek <saitsimsek93@gmail.com>
2026-03-02 16:07:43 +0300

    Fix duplicate heading in HomePage component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import { ROUTES } from '../../constants/routes'
 export const HomePage = () => (
   <main className="page">
     <h1>Revoderm Frontend</h1>
+      <h1>Revoderm Frontend</h1>
     <p>React + TypeScript starter with auth hooks, services and routing.</p>
     <Link className="button" to={ROUTES.LOGIN}>
       Go to Login
